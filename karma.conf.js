aa8e36b23c5e4f3cb0264401b690183fa3ee528d
Pine Mizune <pinemz@gmail.com>
2016-08-23 01:08:15 +0900

    feat(sourcelabs): use soucelabs

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,59 @@
+'use strict'
+
+const path = require('path')
+
+const customLaunchers = {
+  sl_chrome: {
+    base: 'SauceLabs',
+    browserName: 'chrome',
+    platform: 'Windows 10',
+    version: '51.0'
+  },
+  sl_firefox: {
+    base: 'SauceLabs',
+    browserName: 'firefox',
+    platform: 'Windows 10',
+    version: '47.0'
+  },
+  sl_ie: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer',
+    platform: 'Windows 7',
+    version: '11.0',
+  },
+  sl_edge: {
+    base: 'SauceLabs',
+    browserName: 'MicrosoftEdge',
+    platform: 'Windows 10',
+    version: '13.10586'
+  },
+  sl_safari: {
+    base: 'SauceLabs',
+    browserName: 'safari',
+    platform: 'OS X 10.11',
+    version: '9.0',
+  },
+}
+
+const isTravis   = process.env.TRAVIS === 'true'
+const isFirstJob = /\.1$/.test(process.env.TRAVIS_JOB_NUMBER)
+
+const browsers = ['PhantomJS2']
+if (isTravis && isFirstJob) {
+  Array.prototype.push.apply(browsers, Object.keys(customLaunchers))
+}
+
 module.exports = function(config) {
   config.set({
     basePath: '',
-    browsers: ['PhantomJS'],
     frameworks: ['tap', 'sinon'],
+
+    browsers: browsers,
+    customLaunchers: customLaunchers,
+    sauceLabs: {
+      public: 'public',
+    },
+    concurrency: isTravis ? 1 : Infinity,
 
     files: [
       'test.js'
