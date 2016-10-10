af6ba76ef73e9f653b18d63e55bd4b9f72d0a74a
Pine Mizune <pinemz@gmail.com>
2016-10-11 01:02:00 +0900

    test(v2): add vue v2 tests

--- test.js
+++ test.js
@@ -1,28 +1,38 @@
 var expect = require('chai').expect
 var assign = require('object-assign')
 
-function make() {
+var template = '<button type="button" data-sharer="twitter">button</button>'
+
+function makeV1() {
   var wrapper = document.createElement('div')
-  wrapper.innerHTML = '<button type="button" data-sharer="twitter">button</button>'
+  wrapper.innerHTML = template
   return assign({ el: wrapper.querySelector('button') }, require('./index'))
+}
+
+function makeV2() {
+  var wrapper = document.createElement('div')
+  wrapper.innerHTML = template
+  return {
+    component: assign(window, require('./index')),
+    el: wrapper.querySelector('button'),
+  }
 }
 
 describe('index', function () {
   var sandbox
   var spy
-  var sharer
 
   beforeEach(function () {
     sandbox = sinon.sandbox.create()
     spy     = sandbox.spy(window, 'open')
-    sharer  = make()
   })
 
   afterEach(function () {
     sandbox.restore()
   })
 
-  it('bind', function (cb) {
+  it('bind (v1)', function (cb) {
+    var sharer = makeV1()
     sharer.bind()
     sharer.el.click()
 
@@ -32,9 +42,33 @@ describe('index', function () {
     }, 100)
   })
 
-  it('unbind', function (cb) {
+  it('bind (v2)', function (cb) {
+    var sharer = makeV2()
+    sharer.component.bind(sharer.el)
+    sharer.el.click()
+
+    setTimeout(function () {
+      expect(spy.calledOnce).to.be.ok
+      cb()
+    }, 100)
+  })
+
+  it('unbind (v1)', function (cb) {
+    var sharer = makeV1()
     sharer.bind()
     sharer.unbind()
+    sharer.el.click()
+
+    setTimeout(function () {
+      expect(spy.called).to.not.be.ok
+      cb()
+    }, 100)
+  })
+
+  it('unbind (v2)', function (cb) {
+    var sharer = makeV2()
+    sharer.component.bind(sharer.el)
+    sharer.component.unbind(sharer.el)
     sharer.el.click()
 
     setTimeout(function () {
