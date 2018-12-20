80ef32a57b0f3ac492f185fb1076012a66108d80
Pine Mizune <pinemz@gmail.com>
2018-12-20 23:16:14 +0900

    fix(karma): good bye phantomjs

--- karma.conf.js
+++ karma.conf.js
@@ -35,12 +35,12 @@ const customLaunchers = {
   },
 }
 
-const isTravis   = process.env.TRAVIS === 'true'
+const isTravis = process.env.TRAVIS === 'true'
 const isFirstJob = /\.1$/.test(process.env.TRAVIS_JOB_NUMBER)
 
-const browsers = ['PhantomJS']
+const browsers = [ isTravis ? 'ChromiumHeadless' : 'ChromeHeadless' ]
 if (isTravis && isFirstJob) {
-  Array.prototype.push.apply(browsers, Object.keys(customLaunchers))
+  browsers.push(...Object.keys(customLaunchers))
 }
 
 module.exports = function(config) {
