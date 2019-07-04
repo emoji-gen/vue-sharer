4a4d1adf5b750d4e741e968636e9caa226054c16
Pine Mizune <pinemz@gmail.com>
2019-07-05 00:36:54 +0900

    refactor: remove sauce labs

--- karma.conf.js
+++ karma.conf.js
@@ -2,59 +2,15 @@
 
 const path = require('path')
 
-const customLaunchers = {
-  sl_chrome: {
-    base: 'SauceLabs',
-    browserName: 'chrome',
-    platform: 'Windows 10',
-    version: '51.0'
-  },
-  sl_firefox: {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    platform: 'Windows 10',
-    version: '47.0'
-  },
-  sl_ie: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 7',
-    version: '11.0',
-  },
-  sl_edge: {
-    base: 'SauceLabs',
-    browserName: 'MicrosoftEdge',
-    platform: 'Windows 10',
-    version: '13.10586'
-  },
-  sl_safari: {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'OS X 10.11',
-    version: '9.0',
-  },
-}
-
-const isTravis = process.env.TRAVIS === 'true'
-const isFirstJob = /\.1$/.test(process.env.TRAVIS_JOB_NUMBER)
-
-const browsers = [ 'ChromeHeadless' ]
-if (isTravis && isFirstJob) {
-  browsers.push(...Object.keys(customLaunchers))
-}
-
 module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['mocha', 'chai', 'sinon'],
-
-    browsers: browsers,
-    customLaunchers: customLaunchers,
-
+    browsers: ['ChromeHeadless'],
     sauceLabs: {
       public: 'public',
     },
-    concurrency: isTravis ? 5 : Infinity,
+    concurrency: 1,
     browserDisconnectTimeout: 10000,
     browserNoActivityTimeout: 10000,
     browserDisconnectTolerance: 16,
